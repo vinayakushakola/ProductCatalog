9c58ca297cd3ded2979ed4ecc17d2e6a7396cae1
Vinayak Ushakola <vinayak.ushakola@gmail.com>
2024-12-03 19:41:00 +0530

    Marked FakeStoreProductService as default injectior

--- src/main/java/dev/vinayak/ProductCatalog/service/FakeStoreProductService.java
+++ src/main/java/dev/vinayak/ProductCatalog/service/FakeStoreProductService.java
@@ -5,6 +5,7 @@ import dev.vinayak.ProductCatalog.dtos.GenericProductDto;
 import dev.vinayak.ProductCatalog.exceptions.NotFoundException;
 import dev.vinayak.ProductCatalog.thirdpartyclients.productservice.fakestore.FakeStoreProductServiceClient;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
+@Primary
 @Service("fakeStoreProductService")
 public class FakeStoreProductService implements ProductService{
 
