3898a95b5bdad45e0c45911c86c4effd2d9c73ea
Vinayak Ushakola <vinayak.ushakola@gmail.com>
2024-12-15 16:26:35 +0530

    Added custom query interface

---
+++ src/main/java/dev/vinayak/ProductCatalog/repositories/CustomQueries.java
@@ -0,0 +1,7 @@
+package dev.vinayak.ProductCatalog.repositories;
+
+public interface CustomQueries {
+
+    String FIND_ALL_BY_TITLE = "select * from product join product_orders " +
+            "on product.id = product_orders.product_id where title = :naman";
+}
